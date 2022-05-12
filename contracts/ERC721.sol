e085e79ae776f1fd5d942fa80b96d961640a196c
Azharul Haque <phpgladiator@gmail.com>
2022-05-12 16:06:16 +0600

    Update ERC721.sol
    new feature added

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,11 +1,17 @@
 pragma solidity ^0.8.2;
 
 contract ERC721 {
-    
+
     mapping(address => uint256) internal _balances;
+    
     mapping(uint256 => address) internal _owoners;
+    
+    mapping(address => mapping(address => bool )) private _operatorApprovals;
 
+    mapping(uint256 => address) private _tokenApprovals;
 
+    event ApprovalForAll(address indexed _owoner, address indexed operator, bool _approved);
+    event Approval(address _owoner, address _approved, uint256 _tokenId);
 
     // Returns the number of NFT's assigned to an owoner
     function balanceOf(address owoner) public view returns(uint256){
@@ -20,5 +26,24 @@ contract ERC721 {
         return owoner;
     }
 
+    /// Enable or disable an oparetor to manage all of msg.senders assets
+    function setApprovalForAll( address operator, bool approved) public {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    /// Check if an address is an oparetor for another address
+    function isApprovedForAll(address owoner, address operator) public view returns(bool) {
+        return _operatorApprovals[owoner][operator];
+    }
+
+    //Updates an approves address for NFT
+    function approve(address to, uint256 tokenId) public {
+        address owoner = owonerOf(tokenId);
+        _tokenApprovals[tokenId] = to;
+        emit Approval(msg.sender, to, tokenId);
+    }
+
+    // function getApproved();
 }
 
