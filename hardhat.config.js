14698e11e0ac0b131da4167fdf439dcf3cbac201
Azharul Lincoln <>
2022-05-23 20:01:39 +0600

    config changed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -19,13 +20,21 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.2",
   networks: {
-    mumbai: {
-      url: process.env.MUMBAI_RPC,
-      account: [process.env.PRIVATE_KEY],
+    matic: {
+      // url: process.env.MUMBAI_RPC,
+      // accounts: [process.env.PRIVATE_KEY],
+      url: `https://matic-mumbai.chainstacklabs.com`,
+      accounts: [
+        `63f7c35a541128d7877571780dd749613214afefb892af9377831b746aaafcfb`,
+      ],
     },
-    rinkeby: {
-      url: process.env.RINKBE_URL,
-      account: [process.env.PRIVATE_KEY_RINKBE],
+    ropsten: {
+      // url: process.env.ROPSTEN_URL,
+      // account: [process.env.PRIVATE_KEY_ROPSTEN],
+      url: `https://ropsten.infura.io/v3/f5022f29843448fa888b9c147656f66a`,
+      accounts: [
+        `63f7c35a541128d7877571780dd749613214afefb892af9377831b746aaafcfb`,
+      ],
     },
   },
 };
